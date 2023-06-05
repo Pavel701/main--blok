5813f4184e94628dc092233380b0e306121c6bc1
Pavel Sh <paulshipspb@gmail.com>
2023-06-05 06:07:56 +0300

    Составили блок-схему алгоритма

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Ввводим первоначальный массив с клавиатуры
+        Console.WriteLine("Введите элементы массива через запятую:");
+        string input = Console.ReadLine();
+        string[] original = input.Split(',');
+
+        // Считаем количество строк, подходящих по условию
+        int count = 0;
+        foreach (string str in original)
+        {
+            if (str.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        // Создаем новый массив нужной длины
+        string[] result = new string[count];
+
+        // Копируем строки из первоначального массива, подходящие по условию
+        int index = 0;
+        foreach (string str in original)
+        {
+            if (str.Length <= 3)
+            {
+                result[index] = str;
+                index++;
+            }
+        }
+
+// Выводим новый массив на экран
+        foreach (string str in result)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
